466a01e4997b45b0e80a8880c30e17cc1faa91e3
imjeffreylee <imjeffreylee@gmail.com>
2020-08-03 23:49:21 -0400

    Adjusted styling and refactor js

--- js/main.js
+++ js/main.js
@@ -9,18 +9,22 @@ document.addEventListener("DOMContentLoaded", () => {
     const search = (val, results) => {
         if (val === "") {
             renderTable(results);
+            alert("沒有你要找的菜ㄟ歹勢");
             return;
         }
         selected = [];
         for (let i = 0; i < results.length; i++) {
-            const name = results[i][" 品名"];
-            const market = results[i][" 市場"];
-            const kind = results[i][" 種類"];
-            if (name.includes(val) ||
-                market.includes(val) ||
-                kind.includes(val)) selected.push(results[i]);
+            const name = results[i]["品名"];
+            const market = results[i]["市場"];
+            if (name.includes(val) || market.includes(val)) selected.push(results[i]);
         }
-        selected.length ? renderTable(selected) : renderTable(results);
+        
+        if (selected.length) {
+            renderTable(selected);
+        } else {
+            renderTable(results);
+            alert("沒有你要找的菜ㄟ歹勢");
+        }
     }
 
     const handleSort = (dataset) => {
@@ -30,12 +34,12 @@ document.addEventListener("DOMContentLoaded", () => {
             if (dataset.order === "asc") {
                 price.dataset.order = "desc";
                 arrToSort = arrToSort.sort(
-                    (a, b) => b[" 平均(元 / 公斤)"] - a[" 平均(元 / 公斤)"]
+                    (a, b) => b["平均(元 / 公斤)"] - a["平均(元 / 公斤)"]
                 );
             } else {
                 price.dataset.order = "asc";
                 arrToSort = arrToSort.sort(
-                    (a, b) => a[" 平均(元 / 公斤)"] - b[" 平均(元 / 公斤)"]
+                    (a, b) => a["平均(元 / 公斤)"] - b["平均(元 / 公斤)"]
                 );
             }
         } else {
@@ -56,10 +60,17 @@ document.addEventListener("DOMContentLoaded", () => {
     
     const getData = async () => {
         const response = await fetch(
-            "https://data.taipei/api/v1/dataset/f4f80730-df59-44f9-bfb8-32c136b1ae68?scope=resourceAquire"
+            "https://data.taipei/api/v1/dataset/f4f80730-df59-44f9-bfb8-32c136b1ae68?scope=resourceAquire&limit=10"
         );
         const data = await response.json();
-        results = data.result.results.slice(0, 20);
+        let rawData = data.result.results;
+        for (vege of rawData) {
+            let trimmed = JSON.parse(
+                JSON.stringify(vege).replace(/"\s+|\s+"/g, '"')
+            );
+            results.push(trimmed);
+        }
+        console.log(results);
     }
 
     const renderTable = (results) => {
@@ -68,10 +79,10 @@ document.addEventListener("DOMContentLoaded", () => {
         results.forEach((row) => {
             let rowToRender = `
                 <tr>
-                    <td>${row[" 品名"]}</td>
-                    <td>${row[" 市場"]}</td>
-                    <td>${row[" 平均(元 / 公斤)"]}</td>
-                    <td>${row[" 種類"]}</td>
+                    <td>${row["品名"]}</td>
+                    <td>${row["市場"]}</td>
+                    <td>${row["平均(元 / 公斤)"]}</td>
+                    <td>${row["種類"]}</td>
                     <td>${row["日期"]}</td>
                 </tr>
             `;
@@ -92,6 +103,11 @@ document.addEventListener("DOMContentLoaded", () => {
     searchBtn.addEventListener("click", (e) => {
         e.preventDefault();
         search(input.value, results);
+    })
+
+    input.addEventListener("keyup", e => {
+        e.preventDefault();
+        if (e.keyCode === 13) searchBtn.click();
     })
     
     getData().then(() => {
